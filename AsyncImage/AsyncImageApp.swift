746e55eedf884449e07a1a8b4c9f675f36952141
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-04-13 16:09:07 +0600

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Shah Md Imran Hossain on 13/4/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
